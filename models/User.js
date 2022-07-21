4ccdc053ebaeb8e254461877386cc96ec4200ca6
unknown <mcsalazar122@gmail.com>
2022-07-20 21:40:50 -0500

    password encrypt setup with bcrypt

--- models/User.js
+++ models/User.js
@@ -1,9 +1,14 @@
-const bcrypt = require('bcrypt');
 const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
 // create our User model
-class User extends Model {}
+class User extends Model {
+  // set up method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 // create fields/columns for User model
 User.init(
@@ -36,19 +41,17 @@ User.init(
   },
   {
     hooks: {
-      //setup before create lifecycle
+      // set up beforeCreate lifecycle "hook" functionality
       async beforeCreate(newUserData) {
-        newUserData.password = await bcrypt.hash(newUserData.password, 10)
-          return newUserData;
-        
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
       },
-      //update functionality
+
       async beforeUpdate(updatedUserData) {
         updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
         return updatedUserData;
       }
     },
-    
     sequelize,
     timestamps: false,
     freezeTableName: true,
